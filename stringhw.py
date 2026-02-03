543d4e15c48eeed9a6377a15900501764618c73c
13-07-2003 <160623018+13-07-2003@users.noreply.github.com>
2026-02-03 14:15:39 +0530

    stringhw.py

--- stringhw.py
+++ stringhw.py
@@ -1,15 +1,16 @@
-r1, n1, m1 = 101, "Abc", 85
-r2, n2, m2 = 102, "def", 92
-r3, n3, m3 = 103, "ghi", 78
-r4, n4, m4 = 104, "soham", 95
-r5, n5, m5 = 105, "vaishnavi", 88
+r1, n1, m1 = 101, "Adwait", 85     #delcare variable and values
+r2, n2, m2 = 102, "soham", 92
+r3, n3, m3 = 103, "vaishnavi", 78
+r4, n4, m4 = 104, "ishita", 95
+r5, n5, m5 = 105, "saket", 88
 
 print("---------------------------")
-print(f"{'roll'} | {'name'} | {'marks'} |")
+print(f"{'roll':<6} | {'name':<10} | {'marks':<5} |")  #print the header of table
 print("---------------------------")
-print(f"{r1} | {n1}    | {m1} |")
-print(f"{r2} | {n2}      | {m2} |")
-print(f"{r3} | {n3}  | {m3} |")
-print(f"{r4} | {n4}    | {m4} |")
-print(f"{r5} | {n5}    | {m5} |")
+print(f"{r1:<6} | {n1:<10} | {m1:<5} |")      #print the variables with equal distance
+print(f"{r2:<6} | {n2:<10} | {m2:<5} |")
+print(f"{r3:<6} | {n3:<10} | {m3:<5} |")
+print(f"{r4:<6} | {n4:<10} | {m4:<5} |")
+print(f"{r5:<6} | {n5:<10} | {m5:<5} |")
+
 print("---------------------------")
